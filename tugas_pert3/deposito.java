308c37142c51aba5e4d045718dbfba52f6b49668
b1m4s3n4 <64942799+bimasena321@users.noreply.github.com>
2022-10-24 19:09:06 +0700

    Update deposito.java

--- tugas_pert3/deposito.java
+++ tugas_pert3/deposito.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class deposito {
+    public static void main(String[] args) {
+        int u,m,l;
+        int b = 22 / 100;
+        Scanner v = new Scanner(System.in);
+        System.out.println("jumlah awal: ");
+        m = v.nextInt();
+        System.out.println("lama tahun: ");
+        l = v.nextInt();
+        
+        u = m * (1+b)*l;
+        System.out.println("hasil: Rp."+u);
+        
+    }
+}
 
